0ec00b564199db072a40b0b5a51b69fc4159b1c8
duttaanirban <duttaanirbanad@gmail.com>
2025-07-07 21:05:00 +0530

    login dashboard created

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -59,8 +59,42 @@ export async function signup(req, res) {
 };
 
 export async function login(req, res) {
-    // Handle user login logic here
-    res.send('Login Route');
+    try {
+        const {email, password} = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({message: "Email and password are required"});
+        }
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({message: "Invalid email or password"});
+        }
+
+        const isPasswordCorrect = await user.matchPassword(password);
+        if (!isPasswordCorrect) {
+            return res.status(400).json({message: "Invalid password"});
+        }
+
+        const token = jwt.sign({userId: newUser._id}, process.env.JWT_SECRET, {expiresIn: '7d'});
+        
+        res.cookie("jwt", token, {
+            maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+            httpOnly: true,
+            samesite: "strict",
+            secure: process.env.NODE_ENV === "production", // Set to true in production
+        })
+
+        res.status(200).json({
+            success: true,
+            user
+        });
+
+
+    } catch (error) {
+        console.log("Error in controller", error.message);
+        res.status(500).json({message: "Internal server error"});
+    }
 };
 
 export async function logout(req, res) {
