e3bce33ab7e8f15c1b8116151567e9e8f4393921
duttaanirban <duttaanirbanad@gmail.com>
2025-07-09 18:57:31 +0530

    getFriendReqs n getOutgoingReqs updated

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -116,3 +116,36 @@ export async function acceptFriendRequest(req, res) {
         res.status(500).json({message: "Internal server error"});
     }
 };
+
+export async function getFriendRequests(req, res) {
+    try {
+        const incomingReqs = await FriendRequest.find({
+            recipient: req.user.id,
+            status: "pending"
+        }).populate("sender", "fullname profilePicture nativeLanguage learningLanguage");
+
+        const acceptedReqs = await FriendRequest.find({
+            recipient: req.user.id,
+            status: "accepted"
+        }).populate("recipent", "fullname profilePicture");
+
+        res.status(200).json({ incomingReqs, acceptedReqs });
+    } catch (error) {
+        console.log("Error in getFriendRequests controller:", error.message);
+        res.status(500).json({message: "Internal server error"});
+    }
+};
+
+export async function getOutgoingFriendRequests(req, res) {
+    try {
+        const outgoingReqs = await FriendRequest.find({
+            sender: req.user.id,
+            status: "pending"
+        }).populate("recipient", "fullname profilePicture nativeLanguage learningLanguage");
+
+        res.status(200).json(outgoingReqs);
+    } catch (error) {
+        console.log("Error in getOutgoingFriendRequests controller:", error.message);
+        res.status(500).json({message: "Internal server error"});
+    }
+};
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,6 +1,6 @@
 import e from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getRecommendedUsers, getMyFriends, sendFriendRequest, acceptFriendRequest} from "../controllers/user.controller.js";
+import { getRecommendedUsers, getMyFriends, sendFriendRequest, acceptFriendRequest, getFriendRequests, getOutgoingFriendRequests} from "../controllers/user.controller.js";
 const router = e.Router();
 
 // apply auth middleware to all routes in this router
@@ -12,6 +12,7 @@ router.get("/friends", getMyFriends);
 router.post("/friend-request/:id", sendFriendRequest);
 router.put("/friend-request/:id/accept", acceptFriendRequest);
 
-
+router.get("/friend-requests", getFriendRequests);
+router.get("/outgoing-friend-requests", getOutgoingFriendRequests);
 
 export default router;
