3d32578f900b85bacd5d10a578a83fb208cddb89
duttaanirban <duttaanirbanad@gmail.com>
2025-08-01 21:24:35 +0530

    made it for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,10 +6,13 @@ import chatRoutes from './routes/chat.route.js';
 import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import path from 'path';
 
 
 const app = express();
 const PORT = process.env.PORT;
+
+const __dirname = path.resolve();
 
 
 
@@ -22,6 +25,13 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    });
+}
 
 
 app.listen(PORT, () => {
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
+const baseURL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api"; // Use environment variable or default to localhost
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: baseURL,
   withCredentials: true // Adjust the base URL as needed
 });
 
